32a7e414efebed7ac7fb9bd346f9b345ab659d02
Katherine <remick.ka@gmail.com>
2022-07-28 09:59:50 -0700

    merged to main

--- Cardinal/src/main/java/com/nmk/cardinal/security/SecurityConfig.java
+++ Cardinal/src/main/java/com/nmk/cardinal/security/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 	@Autowired
 	private PasswordEncoder encoder;
 	
-
+		@Bean
 		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 			http.csrf().disable()
 			.authorizeRequests()
