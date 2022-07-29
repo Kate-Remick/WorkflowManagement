e6f340fbd6f39b912388506ba8f236cab11008e2
Katherine <remick.ka@gmail.com>
2022-07-29 08:38:44 -0700

    finished service interfaces again

--- Cardinal/src/main/java/com/nmk/cardinal/services/ChatService.java
+++ Cardinal/src/main/java/com/nmk/cardinal/services/ChatService.java
@@ -12,6 +12,12 @@ public interface ChatService {
 
 	public Chat createChat(Chat newChat, String username, Workspace workspace, List<String> users);
 	
+	public Chat addUsers(Chat chat, List<String> users, String username);
+	
+	public Chat addUser(Chat chat, String newUsername, String username);
+	
+	public void leaveChat(Chat chat, String username);
+	
 	
 	
 	
