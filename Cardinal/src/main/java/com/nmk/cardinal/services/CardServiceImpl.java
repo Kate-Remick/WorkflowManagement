3d15db2451e74471d5b02e76d4c2e811182c7a21
Katherine <remick.ka@gmail.com>
2022-07-31 09:05:08 -0700

    fixing commit problems

--- Cardinal/src/main/java/com/nmk/cardinal/services/CardServiceImpl.java
+++ Cardinal/src/main/java/com/nmk/cardinal/services/CardServiceImpl.java
@@ -2,21 +2,34 @@ package com.nmk.cardinal.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nmk.cardinal.entities.Card;
 import com.nmk.cardinal.entities.Deck;
+import com.nmk.cardinal.entities.User;
+import com.nmk.cardinal.repositories.CardRepository;
+import com.nmk.cardinal.repositories.UserRepository;
 
 
 
 @Service
 public class CardServiceImpl implements CardService{
 
+	@Autowired
+	private CardRepository cardRepo;
+	
+	@Autowired
+	private UserRepository userRepo;
+	
 	@Override
 	public List<Card> getCardsByDeck(int deckId, String username) {
 		List<Card> cards = null;
-		
-		return ;
+		User user = userRepo.findByUsername(username);
+		if(user != null) {
+			cards = 
+		}
+		return cards;
 	}
 
 	@Override
