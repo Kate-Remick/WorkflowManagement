6907f047d85bb4be4e25ecb89eb4482f633e46c9
Katherine <remick.ka@gmail.com>
2022-07-31 08:53:23 -0700

    work on card controller

---
+++ Cardinal/src/main/java/com/nmk/cardinal/controllers/CardController.java
@@ -0,0 +1,10 @@
+package com.nmk.cardinal.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin({ "*", "http://localhost:4200" })
+@RestController
+public class CardController {
+
+}
