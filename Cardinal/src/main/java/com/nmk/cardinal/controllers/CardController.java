192445898c8aa9585f32b854273d40c23b2558ed
Katherine <remick.ka@gmail.com>
2022-07-31 09:55:55 -0700

    controllers

--- Cardinal/src/main/java/com/nmk/cardinal/controllers/CardController.java
+++ Cardinal/src/main/java/com/nmk/cardinal/controllers/CardController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nmk.cardinal.entities.Card;
@@ -27,7 +29,7 @@ public class CardController {
 	
 	
 	@GetMapping("cards/deck/{id}")
-	private List<Card> getCardsInDeck(@PathVariable("id") int id, HttpServletResponse res, Principal principal){
+	public  List<Card> getCardsInDeck(@PathVariable("id") int id, HttpServletResponse res, Principal principal){
 		List<Card> cards = null;
 		try {
 			cards = cardServ.getCardsByDeck(id, principal.getName());
@@ -37,6 +39,33 @@ public class CardController {
 			res.setStatus(400);
 		}
 		return cards;
+	}
+	
+	@GetMapping("cards/assigned")
+	public List<Card> getAssigned( HttpServletResponse res, Principal principal){
+		List<Card> cards = null;
+		try {
+			cards = cardServ.getCardsAssigned(principal.getName());
+			res.setStatus(200);
+		}catch(Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+		}
+		return cards;
+		
+	}
+		
+	@PostMapping("cards")
+	public Card create(HttpServletResponse res, Principal principal, @RequestBody Card card) {
+		try {
+			card = cardServ.createCard(card, principal.getName());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+		}
+		return card;
+		
 	}
 	
 	
