b1df00e776a10817f6246268255d5af06a75604a
Katherine <remick.ka@gmail.com>
2022-07-31 09:35:51 -0700

    cardServiceImpl complete

--- Cardinal/src/main/java/com/nmk/cardinal/services/CardServiceImpl.java
+++ Cardinal/src/main/java/com/nmk/cardinal/services/CardServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nmk.cardinal.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.nmk.cardinal.entities.Card;
 import com.nmk.cardinal.entities.Deck;
 import com.nmk.cardinal.entities.User;
+import com.nmk.cardinal.entities.Workspace;
 import com.nmk.cardinal.repositories.CardRepository;
 import com.nmk.cardinal.repositories.UserRepository;
 
@@ -27,51 +29,81 @@ public class CardServiceImpl implements CardService{
 		List<Card> cards = null;
 		User user = userRepo.findByUsername(username);
 		if(user != null) {
-			cards = 
+			cards = cardRepo.findByDeck_IdEquals(deckId);
 		}
 		return cards;
 	}
 
 	@Override
 	public List<Card> getCardsAssigned(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = userRepo.findByUsername(username);
+		return cardRepo.findByAssignedUserEquals(user);
 	}
 
 	@Override
 	public Card createCard(Card newCard, String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return cardRepo.saveAndFlush(newCard);
 	}
 
 	@Override
 	public boolean deleteCard(Card card, String username) {
-		// TODO Auto-generated method stub
-		return false;
+		User user = userRepo.findByUsername(username);
+		if(user != null) {
+			cardRepo.delete(card);
+		}
+		return !cardRepo.equals(card);
 	}
 
 	@Override
 	public Card editCard(Card currentCard, Card newCard, String username) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = userRepo.findByUsername(username);
+		if(user != null) {
+			currentCard.setName(newCard.getName());
+			currentCard.setDueDate(newCard.getDueDate());
+			currentCard.setDescription(newCard.getDescription());
+			currentCard = cardRepo.saveAndFlush(currentCard);
+		}
+		
+		return currentCard;
 	}
 
 	@Override
 	public Card completeCard(Card card, String username) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = userRepo.findByUsername(username);
+		if(user != null) {
+			card.setCompleted(true);
+			card.setCompletedAt(LocalDateTime.now());
+			card = cardRepo.saveAndFlush(card);
+		}
+		
+		return card;
 	}
 
 	@Override
 	public Card moveCardToNewDeck(Card card, Deck newDeck, String username) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = userRepo.findByUsername(username);
+		if(user != null) {
+			card.setDeck(newDeck);
+			card = cardRepo.saveAndFlush(card);
+		}
+		
+		
+		return card;
 	}
 
 	@Override
-	public Card assignCard(Card card, String username, String managerUsername) {
-		// TODO Auto-generated method stub
-		return null;
+	public Card assignCard(Card card, String username, String managerUsername, Workspace workspace) {
+		User manager = userRepo.findByUsername(managerUsername);
+		if(workspace.getManager().equals(manager)) {
+			User user = userRepo.findByUsername(username);
+			if(user != null) {
+				card.setAssignedUser(user);
+				card = cardRepo.saveAndFlush(card);
+			}
+		}
+		
+		
+		return card;
 	}
 	
 	
