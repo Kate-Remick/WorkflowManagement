f201525a59c326a24c853ca6d81c35560cfc3e2c
Katherine <remick.ka@gmail.com>
2022-08-02 10:41:45 -0700

    completed chat service angular

--- ngCardinal/src/app/services/card.service.ts
+++ ngCardinal/src/app/services/card.service.ts
@@ -1,13 +1,86 @@
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
+import { identifierName } from '@angular/compiler';
+import { catchError, throwError, Observable } from 'rxjs';
+import { Card } from '../models/card';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
 
-  constructor() { }
+  private url = environment.baseUrl + "api/cards";
+
+  constructor(private http: HttpClient, private auth: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
 
   getCredentials(): string | null {
     return localStorage.getItem('credentials');
   }
+
+  getCardsInDeck(id: number):Observable<Card[]>{
+    return this.http.get<Card[]>(this.url + "/" + id, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+  getAssignedCards():Observable<Card[]>{
+    return this.http.get<Card[]>(this.url + "/assigned", this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+  createCard(card : Card):Observable<Card>{
+    return this.http.post<Card>(this.url,card, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+  deleteCard(id: number):Observable<void>{
+    return this.http.delete<void>(this.url + "/" + id, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+  updateCard(card: Card, id : number):Observable<Card>{
+    return this.http.put<Card>(this.url + "/" + id, card ,this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+
+
+
+
 }
--- ngCardinal/src/app/services/chat.service.ts
+++ ngCardinal/src/app/services/chat.service.ts
@@ -1,13 +1,74 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Chat } from '../models/chat';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  private url = environment.baseUrl + "api/cards";
 
-  constructor() { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
 
   getCredentials(): string | null {
     return localStorage.getItem('credentials');
   }
+
+  getChatsInWorkspace(id: number):Observable<Chat[]>{
+    return this.http.get<Chat[]>(this.url + "/" + id, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+
+  getUserChats():Observable<Chat[]>{
+    return this.http.get<Chat[]>(this.url, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+
+  leaveChat(chat: Chat): Observable<Chat>{
+    return this.http.get<Chat>(this.url + "/leave", this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+
+  addUsers(chat: Chat, id: number): Observable<Chat>{
+    return this.http.get<Chat>(this.url + "/" + id + "/addUsers",  ,this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error(' error: ' + err)
+      );
+      })
+    );
+  }
+
 }
